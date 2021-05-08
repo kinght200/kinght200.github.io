85b8b09a773171186651e62ca2acccfe98006fa6
kinght200 <1759552380@qq.com>
2021-05-08 18:29:47 +0800

    默认的

--- 爬虫/第一章/03-requests入门_02.py
+++ 爬虫/第一章/03-requests入门_02.py
@@ -10,5 +10,5 @@ dat = {
 
 # 发送post请求，发送的数据必须放在字典中，通过data参数进行传递
 resp = requests.post(url, data=dat)
-print(resp.json())  # 将服务器返回的内容直接处理城json()
+print(resp.json())  # 将服务器返回的内容直接处理成json()
 resp.close()

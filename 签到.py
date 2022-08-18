8c0bdad089f20d44d00a7dd38daad91643865622
kinght200 <1759552380@qq.com>
2022-08-18 14:46:19 +0800

    Test

--- 签到.py
+++ 签到.py
@@ -4,11 +4,16 @@ from fake_useragent import UserAgent
 url = 'http://www.baidu.com/'
 
 # 浏览器请求头
-headers = {
-    'User-agent': UserAgent().chrome
-}
+# headers = {
+#     'User-agent': UserAgent().chrome
+# }
+#
+# reqs = requests.get(url=url, headers=headers)
+# reqs.encoding = 'utf-8'
+# ques = reqs.text
+# print(ques)
 
-reqs = requests.get(url=url, headers=headers)
-reqs.encoding = 'utf-8'
-ques = reqs.text
-print(ques)
+def fun1():
+    return 'hello','world'
+
+print(fun1())
